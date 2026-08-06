72b2e0e13798112347c5d34c813f337c4497130b
PIYUSH PAL <piyushpalc@gmail.com>
2026-08-06 22:41:58 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,21 @@
+public class hello{
+    public static void main(String[] args){
+        int a = 10;
+        int d = 5;
+        float b = 5.5f;
+        double c = 20.5;
+        char grade = 'A';
+        System.out.println("hello world");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("grade: " + grade);
+        System.out.println("greater than 10 " + (a > 10));
+        System.out.println("modulo " + (a % d));
+        System.out.println("equal " + (a == d));
+        System.out.println("not equal " + (a != d));
+        System.out.println("less than " + (a < d));
+        System.out.println("greater than " + (a > d));
+    }
+}
