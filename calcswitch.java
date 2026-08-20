1839b406d6298d488ecd5b96cf5f4984915799c6
PIYUSH PAL <piyushpalc@gmail.com>
2026-08-20 14:22:52 +0530

    modified output

--- calcswitch.java
+++ calcswitch.java
@@ -14,16 +14,16 @@ public class calcswitch {
         int b = sc.nextInt();
         switch(op){
             case '+':
-                System.out.println(a + b);
+                System.out.println("Addition:"+ (a + b));
                 break;
             case '-':
-                System.out.println(a - b);
+                System.out.println("Subtraction:"+ (a - b));
                 break;
             case '*':
-                System.out.println(a * b);
+                System.out.println("Multiplication:"+ (a * b));
                 break;
             case '/':
-                System.out.println(a / b);
+                System.out.println("Division:"+ (a / b));
                 break;
             default:
                 System.out.println("Invalid operator");
