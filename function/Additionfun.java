e7961a51ad475b3e89aea2730fc715310603acfa
PIYUSH PAL <piyushpalc@gmail.com>
2026-08-23 12:33:56 +0530

    new foldeer added

---
+++ function/Additionfun.java
@@ -0,0 +1,23 @@
+package funtion;
+import java.util.*;
+
+public class Additionfun{
+    public static int addition(int a,int b){
+        int sum = a+b;
+        return sum;
+    }
+
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("enter first number:");
+        int a = sc.nextInt();
+
+        System.out.println("enter second number:");
+        int b = sc.nextInt();
+        
+        int sum = addition(a,b);
+
+        System.out.println("sum of two numbers is: "+sum);
+    }
+}
---
+++ function/multi.java
@@ -0,0 +1,25 @@
+package funtion;
+import java.util.*;
+
+public class multi{
+
+    public static int multiplication(int a, int b){
+
+        int mult= a*b;
+        return mult;
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter first number:");
+        int a = sc.nextInt();
+
+        System.out.println("enter second number:");
+        int b = sc.nextInt();
+
+        int mult = multiplication(a,b);
+        System.out.println("multiplication of two numbers is: "+mult);
+    }
+
+}
